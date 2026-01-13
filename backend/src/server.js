1be440d0b738006289d7e6a454227b6e674a363c
pulkitgaba009 <pulkitgaba009@gmail.com>
2026-01-13 12:57:48 +0530

    Redis Rate Limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,28 @@
 import express from "express";
 import questionsRoutes from "./routes/questionsRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv  from "dotenv";
-import settingsRoutes from "./routes/settingsRoutes.js"
-import resultRoutes from "./routes/resultRoutes.js"
+import dotenv from "dotenv";
+import settingsRoutes from "./routes/settingsRoutes.js";
+import resultRoutes from "./routes/resultRoutes.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
-const PORT =  process.env.PORT || 8080;
-
-connectDB();
+const PORT = process.env.PORT || 8080;
 
 // middleware
 app.use(express.json());
 
-// routes
-app.use("/api/questions",questionsRoutes);
-app.use("/api/settings",settingsRoutes);
-app.use("/api/results",resultRoutes);
+// app.use(rateLimiter);
 
-app.listen(PORT,()=>{
+// routes
+app.use("/api/questions", questionsRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/results", resultRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server Started on PORT : ${PORT}`);
-})
+  });
+});
